e653c6549b114c487dd017af625982c2a1e7c0ab
Gurjot Singh Suri <gurjotsingh.suri@mavenir.com>
2022-06-01 12:36:11 -0700

    collections iterable

--- processWebhook.py
+++ processWebhook.py
@@ -1,9 +1,4 @@
-import collections.abc
-#hyper needs the four following aliases to be done manually.
-collections.Iterable = collections.abc.Iterable
-collections.Mapping = collections.abc.Mapping
-collections.MutableSet = collections.abc.MutableSet
-collections.MutableMapping = collections.abc.MutableMapping
+from collections.abc import Iterable
 
 from xml.dom import minidom
 
